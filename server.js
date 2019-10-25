4faca62a811d3aea6f792e42d9cd842f109d1b52
Mona Lisa <divijmathur@gmail.com>
2019-10-25 09:31:09 -0700

    added mongodb db for webscraping

--- server.js
+++ server.js
@@ -1,13 +1,50 @@
+var express = require("express");
+var mongojs = require("mongojs");
 var cheerio = require("cheerio");
 var axios = require("axios");
-axios.get("https://www.cnn.com/world").then(function(response){
-    var $ = cheerio.load(response.data);
-    var results = [];
-    $("span.cd__headline-text").each(function(i,element){
-        var title = $(element).text();
-        results.push({
-            title: title
+var app = express();
+var databaseUrl = "news";
+var collections = ["scrapedNews"];
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error){
+    console.log("database error ", error)
+});
+app.get("/", function(req,res){
+    res.send("Hello world!")
+});
+app.get("/all", function(req,res){
+    db.scrapedNews.find({}, function(error,found){
+        if (error) {
+            console.log(error);
+        }
+        else {
+            res.json(found);
+        }
+    });
+});
+
+app.get("/scrape", function(req,res){
+    axios.get("https://www.cnn.com/world").then(function(response){
+        var $ = cheerio.load(response.data);
+        $("span.cd__headline-text").each(function(i, element){
+            var title = $(element).text();
+            if (title && link) {
+            db.scrapedNews.insert({
+                title: title
+            }, function(err, inserted){
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log(inserted);
+                }
+            });
+            }
         });
     });
-    console.log(results);
+    res.send("scraping done");
+});
+
+app.listen(3000, () => {
+    console.log("server is running");
 });
